730836fb00195877370e74ff5ff097491d200bd3
prajyu <prajyu2003@gmail.com>
2023-08-05 21:25:54 +0530

    slider count error fixed

--- js/segment.js
+++ js/segment.js
@@ -5,33 +5,52 @@ let segment4 = document.querySelector("#segment4");
 let segment5 = document.querySelector("#segment5");
 let segment6 = document.querySelector("#segment6");
 
+let slider1 = false;
+let slider2 = false;
+let slider3 = false;
+let slider4 = false;
+let slider5 = false;
+let slider6 = false;
+
 segment1.addEventListener("click", () => {
+  if (slider1) return;
   setTimeout(() => {
     $("#JiSlider1").JiSlider({ color: "#fff", start: 1, reverse: true });
+    slider1 = true;
   }, 100);
 });
 segment2.addEventListener("click", () => {
+  if (slider2) return;
   setTimeout(() => {
     $("#JiSlider2").JiSlider({ color: "#fff", start: 1, reverse: true });
+    slider2 = true;
   }, 100);
 });
 segment3.addEventListener("click", () => {
+  if (slider3) return;
   setTimeout(() => {
     $("#JiSlider3").JiSlider({ color: "#fff", start: 1, reverse: true });
+    slider3 = true;
   }, 100);
 });
 segment4.addEventListener("click", () => {
+  if (slider4) return;
   setTimeout(() => {
     $("#JiSlider4").JiSlider({ color: "#fff", start: 1, reverse: true });
+    slider4 = true;
   }, 100);
 });
 segment5.addEventListener("click", () => {
+  if (slider5) return;
   setTimeout(() => {
     $("#JiSlider5").JiSlider({ color: "#fff", start: 1, reverse: true });
+    slider5 = true;
   }, 100);
 });
 segment6.addEventListener("click", () => {
+  if (slider6) return;
   setTimeout(() => {
     $("#JiSlider6").JiSlider({ color: "#fff", start: 1, reverse: true });
+    slider6 = true;
   }, 100);
 });
